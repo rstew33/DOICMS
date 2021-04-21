02c970d1156a626b50e684cb11f8e4adbb60ccb1
Andrew Steward <rstew33@gmail.com>
2021-04-20 20:06:21 -0400

    initial commit 2.0

---
+++ DOICMS.Data/Agent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOICMS.Data
+{
+    public class Agent
+    {
+        [Key]
+        public int AgentID { get; set; }
+        [Required] 
+        public int LicenseNumber { get; set; }
+        [Required] 
+        [Display(Name = "Agent Name")]
+        public string Name { get; set; }
+        [Required] 
+        public string Email { get; set; }
+        [Required] 
+        public string Address { get; set; }
+        [Required] 
+        public string PhoneNumber { get; set; }
+        //public virtual List<Insurer> Appointments[fk - Insurers]
+        //public virtual List<Complaint> ComplaintsAgents[fk - Complaints]
+
+    }
+}
