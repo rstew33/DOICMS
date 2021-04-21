87f0de1da83dd06118636c95b1ea31e0df8ff663
Andrew Steward <rstew33@gmail.com>
2021-04-20 20:22:15 -0400

    added Agent functionality

---
+++ DOICMS.WebMVC/Controllers/AgentController.cs
@@ -0,0 +1,34 @@
+﻿using DOICMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DOICMS.WebMVC.Controllers
+{
+    public class AgentController : Controller
+    {
+        [Authorize]
+        // GET: Agent
+        public ActionResult Index()
+        {
+            var model = new AgentListItem[0];
+            return View(model);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AgentCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
+    }
+}
